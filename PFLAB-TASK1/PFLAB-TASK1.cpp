7b38bec2eacf8c5d03f97d3bd2c1c9b2eed23fb0
Hamna-byte <yousafsaadi564@gmail.com>
2024-10-27 21:14:25 +0000

    Add project files.

---
+++ PFLAB-TASK1/PFLAB-TASK1.cpp
@@ -0,0 +1,14 @@
+// PFLAB-TASK1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+
+}
+
